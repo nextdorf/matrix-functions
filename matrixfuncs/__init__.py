c7478b5f10fde6d5ed9b5a1b6da52276bd7e9683
nextdorf <nextdorf@users.noreply.github.com>
2025-02-19 10:24:43 +0100

    Cleaning up old unused code from previous version

--- matrixfuncs/__init__.py
+++ matrixfuncs/__init__.py
@@ -1,6 +1,3 @@
-# from ._sym_krylov import SymKrylovSpaceCache, SymKrylovSpace
-# from ._krylov import KrylovSpace
-# from ._func import MFunc, eigval_multiplicity
 from . import utils
 from .utils import apply_fn, Multiplicity
 from ._mspace import MSpace
--- matrixfuncs/utils/__init__.py
+++ matrixfuncs/utils/__init__.py
@@ -1,5 +1,10 @@
 from ._fcoeffs import (
-  apply_fn, b_matrix, eigval_multiplicity, function_coeffs, matrix_power_series, Multiplicity
+  apply_fn,
+  b_matrix,
+  eigval_multiplicity,
+  function_coeffs,
+  matrix_power_series,
+  Multiplicity
 )
 
 def err(x,y):
